adfe833656f96fa519808731960e632e8f0cd780
harun-ibram <ibramharun7@gmail.com>
2026-07-16 14:27:01 +0300

    Fix link between database and backend.

--- src/server/database.py
+++ src/server/database.py
@@ -1,9 +1,13 @@
 import os
 from collections.abc import Generator
-
+from dotenv import load_dotenv
 from sqlmodel import Session, create_engine
 
+load_dotenv("../../.env")
+
 DATABASE_URL = os.environ.get("DB_URL")
+
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 

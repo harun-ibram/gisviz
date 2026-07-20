6afd06997a72ea1444c0088d8baf8c2e14e32d86
harun-ibram <ibramharun7@gmail.com>
2026-07-20 12:24:07 +0300

    Remove frontend from backend branch.

--- src/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const db_user = process.env.DB_USER;
-export const db_password = process.env.DB_PASSWORD;
-export const db_host = process.env.DB_HOST;
-export const db_port = process.env.DB_PORT;
-export const db_name = process.env.DB_NAME;

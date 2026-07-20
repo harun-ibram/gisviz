63cfd847da268d468da70b7d2a7c0b54ef7ebbed
harun-ibram <ibramharun7@gmail.com>
2026-07-20 13:44:59 +0300

    Connect to cloud database.

--- src/database.py
+++ src/database.py
@@ -4,14 +4,3 @@ from dotenv import load_dotenv
 from sqlmodel import Session, create_engine
 
 load_dotenv("../../.env")
-
-DATABASE_URL = os.environ.get("DB_URL")
-
-print(DATABASE_URL)
-
-engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
-
-
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
--- src/main.py
+++ src/main.py
@@ -16,19 +16,77 @@ from models import (
     Region,
 )
 
+import os
+
+from google.cloud.sql.connector import Connector, IPTypes
+import pg8000
+
+import sqlalchemy
+
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = os.environ[
+        "INSTANCE_CONNECTION_NAME"
+    ]  # e.g. 'project:region:instance'
+    db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
+    db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
+    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector(refresh_strategy="LAZY")
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    # The Cloud SQL Python Connector can be used with SQLAlchemy
+    # using the 'creator' argument to 'create_engine'
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+        # ...
+    )
+    return pool
+
+# Create the engine once at module load / startup, not per-request
+engine = connect_with_connector()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 # FastAPI and middleware
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://gisviz-xi.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-# Type annotation and dependency for Session
-SessionDep = Annotated[Session, Depends(get_session)]
 
 # Helper function for formatting the data in the table into a usable object
 def _row_to_dict(obj: SQLModel, geojson: str | None) -> dict[str, Any]:
